3a95461b7f13715061d6829a8ba5917b4d67843c
jeffyjkang <jeffyjkang@gmail.com>
2019-08-14 14:41:51 -0700

    add config object with env variables

--- howsthewaterfront/src/index.js
+++ howsthewaterfront/src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Amplify from "aws-amplify";
-import config from "./aws-exports";
-Amplify.configure(config);
+// import config from "./aws-exports";
 require("dotenv").config();
+const config = {
+  aws_project_region: process.env.REACT_APP_PROJECT_REGION,
+  aws_cognito_identity_pool_id: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: process.env.REACT_APP_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {
+    domain: process.env.REACT_APP_DOMAIN,
+    scope: [
+      "phone",
+      "email",
+      "openid",
+      "profile",
+      "aws.cognito.signin.user.admin"
+    ],
+    redirectSignIn: "http://localhost:3000/",
+    redirectSignOut: "http://localhost:3000/",
+    responseType: "code"
+  },
+  federationTarget: "COGNITO_USER_POOLS"
+};
+Amplify.configure(config);
 
 ReactDOM.render(<App />, document.getElementById("root"));
