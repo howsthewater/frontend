01cf3634ad2f193935a2803e4c700fd1e47e1afa
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-08-26 21:25:58 -0500

    Set up the redux store

--- howsthewaterfront/src/index.js
+++ howsthewaterfront/src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/index.css";
 import App from "./components/App";
-require("dotenv").config();
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
