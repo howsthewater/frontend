bc41ebdb129800e3716ed8861ede6e42bbb08948
Jonathan Daly <dalyj138@gmail.com>
2019-08-28 18:19:06 -0400

    Removed packages Reactstrap/Bootstrap, and added apollo-boost, @apollo/react-hooks, and graphql.

--- howsthewaterfront/src/components/App.js
+++ howsthewaterfront/src/components/App.js
@@ -2,12 +2,29 @@ import React from "react";
 import "../styles/App.css";
 import Search from "./Search";
 
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'howsthewaterfeature.herokuapp.com/graphql'
+});
+
+function GetLocations() {
+  const { loading, error, data } = useQuery(gql`
+    {
+
+    }
+  `)
+}
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>How's The Water</h1>
-      <Search />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>How's The Water</h1>
+        <Search />
+      </div>
+    </ApolloProvider>
   );
 };
 
