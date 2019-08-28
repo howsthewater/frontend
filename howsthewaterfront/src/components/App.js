c1ae1604e1b81bb4da14fec8c0474b431ce23adb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-08-27 19:30:18 -0500

    Added routes - landing, login, signup, searchresult. Also created privateroute for home. If not authenticated it will redirect to login page

--- howsthewaterfront/src/components/App.js
+++ howsthewaterfront/src/components/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "../styles/App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginForm from "./Login";
+import LandingForm from "./Landing";
+import SignUpForm from "./SignUp";
+import SearchResultForm from "./SearchResult";
+import Routes from "./Routes";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>How's The Water</h1>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated
+    };
+    return (
+      <Router>
+        <Route exact path="/" component={LandingForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/signup" component={SignUpForm} />
+        <Route exact path="/searchresult" component={SearchResultForm} />
+        <Routes childProps={childProps} />
+      </Router>
+    );
+  }
+}
 
 export default App;
