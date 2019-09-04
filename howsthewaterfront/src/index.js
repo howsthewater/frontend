1be8141d9b7c39ec1fd2a5577e99632568cc365d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-09-03 21:53:41 -0500

    Updated the index.js for environment set up

--- howsthewaterfront/src/index.js
+++ howsthewaterfront/src/index.js
@@ -8,9 +8,31 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import Amplify from "aws-amplify";
-import config from "./aws-exports";
+//import config from "./aws-exports";
 import { BrowserRouter as Router } from "react-router-dom";
+require("dotenv").config();
 
+const config = {
+  aws_project_region: process.env.REACT_APP_PROJECT_REGION,
+  aws_cognito_identity_pool_id: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: process.env.REACT_APP_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {
+    domain: process.env.REACT_APP_DOMAIN,
+    scope: [
+      "phone",
+      "email",
+      "openid",
+      "profile",
+      "aws.cognito.signin.user.admin"
+    ],
+    redirectSignIn: "http://localhost:3000/home",
+    redirectSignOut: "http://localhost:3000/",
+    responseType: "code"
+  },
+  federationTarget: "COGNITO_USER_POOLS"
+};
 Amplify.configure(config);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
