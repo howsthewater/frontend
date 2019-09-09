d47151ffc87946638459d2cc7e74cc5c050170fb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-09-09 15:51:59 -0500

    Created the first fold with responsive for tablet. Pending search + mobile responsive header

--- howsthewaterfront/src/components/Landing.js
+++ howsthewaterfront/src/components/Landing.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles/landing.css";
+import logo from "../assets/Logo - htw.png";
+import logoWords from "../assets/Logo - htw - words.png";
 
 const Landing = () => {
   return (
     <body>
-      <header class="header">some text ..</header>
+      <header>
+        <div className="logo-container">
+          <img className="logo" src={logo} alt="How's the water logo" />
+          <img className="logo-txt" src={logoWords} alt="How's the water" />
+        </div>
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Login</a>
+          <button className="signup-button">Sign Up</button>
+        </nav>
+      </header>
+      <section className="top-content"></section>
     </body>
   );
 };
