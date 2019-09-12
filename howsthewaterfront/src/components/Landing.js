cc1ef7ec54c7f778b14e3eb4b22ce9ac2b1d8abb
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-09-12 13:05:00 -0500

    Added comments for landing page component

--- howsthewaterfront/src/components/Landing.js
+++ howsthewaterfront/src/components/Landing.js
@@ -4,19 +4,30 @@ import logo from "../assets/Logo - htw.png";
 import logoWords from "../assets/Logo - htw - words.png";
 import SearchBeach from "./Search";
 
+/*
+ * Landing page - Functional component
+ * Renders the page when the user logs in. Maps to landing.css for styling
+ *
+ */
+
 const Landing = () => {
   return (
     <>
+      {/* Header Content for Landing page */}
       <header>
+        {/* Logo section on the left of header */}
         <div className="logo-container">
           <img className="logo" src={logo} alt="How's the water logo" />
           <img className="logo-txt" src={logoWords} alt="How's the water" />
         </div>
 
+        {/* Navigation section on the right of header */}
+        {/* Hamburger icon for smaller screen size */}
         <label className="hamburger-icon" for="toggle">
           &#9776;
         </label>
         <input type="checkbox" id="toggle" />
+        {/* Navigation links - header - right */}
         <nav className="menu">
           <a href="#">About</a>
           <a href="#">Features</a>
@@ -24,6 +35,8 @@ const Landing = () => {
           <button className="signup-button">Sign Up</button>
         </nav>
       </header>
+
+      {/* Section for first fold - includes a background image and search component */}
       <section className="top-content">
         <div className="search-content">
           <SearchBeach />
