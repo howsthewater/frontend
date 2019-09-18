57bd05756a5fdd18c747cf2469ba3c56d5ce9efe
jamesbieber <jamesbieber97@gmail.com>
2019-09-17 19:46:45 -0700

    Layout of search elements complete with styling. Working on data
    Co-authored-by: James Bieber <jamesbieber97@gmail.com>
    Co-authored-by: Jeffrey Kang <jeffyjkang@gmail.com>

--- howsthewaterfront/src/components/AdvancedSearch.js
+++ howsthewaterfront/src/components/AdvancedSearch.js
@@ -11,16 +11,99 @@ import picnicIcon from "../assets/icons8-picnic-table-50.png";
 import surf_header from "../assets/beach_surf_header.jpg";
 import logo from "../assets/Logo - htw.png";
 import logoWords from "../assets/Logo - htw - words.png";
+// graphql stuff
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 import "../styles/advanced-search.css";
 
+const dummyData = [
+  {
+    NameMobileWeb: "aName",
+    DISTRICT: "aRegion",
+    RESTROOMS: "Yes",
+    PARKING: "No",
+    DSABLDACSS: "Yes",
+    PCNC_AREA: "Yes",
+    VOLLEYBALL: "No",
+    DOG_FRIENDLY: "No",
+    EZ4STROLLERS: "Yes",
+    windspeed: "something",
+    windDirection: "something else",
+    swellheight: "swell",
+    temp: "95"
+  },
+  {
+    NameMobileWeb: "aName",
+    DISTRICT: "aRegion",
+    RESTROOMS: "Yes",
+    PARKING: "No",
+    DSABLDACSS: "Yes",
+    PCNC_AREA: "Yes",
+    VOLLEYBALL: "No",
+    DOG_FRIENDLY: "No",
+    EZ4STROLLERS: "Yes",
+    windspeed: "something",
+    windDirection: "something else",
+    swellheight: "swell",
+    temp: "95"
+  },
+  {
+    NameMobileWeb: "aName",
+    DISTRICT: "aRegion",
+    RESTROOMS: "Yes",
+    PARKING: "No",
+    DSABLDACSS: "Yes",
+    PCNC_AREA: "No",
+    VOLLEYBALL: "No",
+    DOG_FRIENDLY: "No",
+    EZ4STROLLERS: "Yes",
+    windspeed: "something",
+    windDirection: "something else",
+    swellheight: "swell",
+    temp: "95"
+  },
+  {
+    NameMobileWeb: "aName",
+    DISTRICT: "aRegion",
+    RESTROOMS: "Yes",
+    PARKING: "No",
+    DSABLDACSS: "Yes",
+    PCNC_AREA: "Yes",
+    VOLLEYBALL: "Yes",
+    DOG_FRIENDLY: "Yes",
+    EZ4STROLLERS: "Yes",
+    windspeed: "something",
+    windDirection: "something else",
+    swellheight: "swell",
+    temp: "95"
+  },
+  {
+    NameMobileWeb: "aName",
+    DISTRICT: "aRegion",
+    RESTROOMS: "Yes",
+    PARKING: "No",
+    DSABLDACSS: "No",
+    PCNC_AREA: "Yes",
+    VOLLEYBALL: "Yes",
+    DOG_FRIENDLY: "Yes",
+    EZ4STROLLERS: "Yes",
+    windspeed: "something",
+    windDirection: "something else",
+    swellheight: "swell",
+    temp: "95"
+  }
+];
+
 const AdvancedSearch = () => {
-  const [searchData, searchBeaches] = useState(null);
+  const [searchData, setSearchBeaches] = useState(null);
 
-  useEffect(() => {}, {});
-
+  useEffect(() => {
+    setSearchBeaches(dummyData);
+  }, {});
+  console.log(searchData);
   return (
-    <div className="sResultContainer">
+    <div className="container">
       Advanced Search Page
       {/****************  SUBJECT TO CHANGE **************/}
       <header>
@@ -50,6 +133,124 @@ const AdvancedSearch = () => {
       <div className="searchBackground">
         <Search />
       </div>
+      <div className="searchResults">
+        {/* Component above results that lays out the columns*/}
+        <div className="searchTable">
+          <h2 className="table-headers beachspot">Beach Spot</h2>
+          <h2 className="table-headers region">Region</h2>
+          <h2 className="table-headers amenities">Amenities</h2>
+          <h2 className="table-headers currentsurf">
+            Current Surf Information
+          </h2>
+        </div>
+        <div className="DataContainer">
+          {/* {data.map=>(div NameMobileWeb , div region, div, amenties, current surf)} */}
+          {searchData
+            ? searchData.map(beach => (
+                <div className="rowContainer" key={Math.random()}>
+                  <div className="beach-spot beach-data">
+                    {beach.NameMobileWeb}
+                  </div>
+                  <div className="beach-region beach-data">
+                    {beach.DISTRICT}
+                  </div>
+                  <div className="beach-amenities beach-data">
+                    <img
+                      className="icons"
+                      src={toiletIcon}
+                      alt="ttIcon"
+                      style={
+                        beach
+                          ? beach.RESTROOMS === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={wheelchairIcon}
+                      alt="wrIcon"
+                      style={
+                        beach
+                          ? beach.DSABLDACSS === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={picnicIcon}
+                      alt="pcIcon"
+                      style={
+                        beach
+                          ? beach.PCNC_AREA === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={strollerIcon}
+                      alt="srIcon"
+                      style={
+                        beach
+                          ? beach.EZ4STROLLERS === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={parkingIcon}
+                      alt="pgIcon"
+                      style={
+                        beach
+                          ? beach.PARKING === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={dogIcon}
+                      alt="dgIcon"
+                      style={
+                        beach
+                          ? beach.DOG_FRIENDLY === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                    <img
+                      className="icons"
+                      src={volleyIcon}
+                      alt="vyIcon"
+                      style={
+                        beach
+                          ? beach.toilet === "Yes"
+                            ? { display: "block" }
+                            : { display: "none" }
+                          : { display: "none" }
+                      }
+                    />
+                  </div>
+                  <div className="beach-currentinfo beach-data">
+                    Wind Speed: {beach.windspeed} | Wind Direction:{" "}
+                    {beach.windDirection} | Swell Height: {beach.swellheight} |
+                    Temp: {beach.temp}
+                  </div>
+                </div>
+              ))
+            : ""}
+        </div>
+      </div>
+      <CopyrightFooter />
     </div>
   );
 };
