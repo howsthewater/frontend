7e02b45c2e7b278270a24eb3787c9c631e868f71
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-03 14:00:08 -0500

    Code clean-up: Replaced the header code with the Header component

--- howsthewaterfront/src/components/ChangePassword.js
+++ howsthewaterfront/src/components/ChangePassword.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 function ChangePassword(props) {
-  return <div> Change Password page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default ChangePassword;
