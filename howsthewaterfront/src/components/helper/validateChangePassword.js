f5390ad82494f718d1745c594bc6201f71bbe4d2
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-03 16:02:55 -0500

    Change Password: Added validations for new password and confirm password and checked the validation and error message display

--- howsthewaterfront/src/components/helper/validateChangePassword.js
+++ howsthewaterfront/src/components/helper/validateChangePassword.js
@@ -1,25 +1,60 @@
 export default function validate(values) {
   let errors = {};
   console.log("-----------IN VALIDATE CHANGE PASSWORD ---------------");
+
   // Validation for old password
   if (!values.oldPassword) {
     //password - more than 8 characters
-    errors.oldPassword = "Password is required";
-  } else if (values.password.length < 8) {
+    errors.oldPassword = "Old Password is required";
+  } else if (values.oldPassword.length < 8) {
     errors.oldPassword = "Password needs to be more than 8 characters";
-  } else if (!/\d/.test(values.password)) {
+  } else if (!/\d/.test(values.oldPassword)) {
     errors.oldPassword = "Password should contain at least one number";
-  } else if (!/[a-z]/.test(values.password)) {
+  } else if (!/[a-z]/.test(values.oldPassword)) {
     errors.oldPassword = "Password should contain at least one lowercase";
-  } else if (!/[A-Z]/.test(values.password)) {
+  } else if (!/[A-Z]/.test(values.oldPassword)) {
     errors.oldPassword = "Password should contain at least one uppercase";
-  } else if (!/\W/.test(values.password)) {
+  } else if (!/\W/.test(values.oldPassword)) {
     errors.oldPassword =
       "Password should contain at least one special character";
   }
 
-  console.log(
-    "ERRORS IS -------------------------------" + JSON.stringify(errors)
-  );
+  // Validation for new password
+  if (!values.newPassword) {
+    //password - more than 8 characters
+    errors.newPassword = "New Password is required";
+  } else if (values.newPassword.length < 8) {
+    errors.newPassword = "Password needs to be more than 8 characters";
+  } else if (!/\d/.test(values.newPassword)) {
+    errors.newPassword = "Password should contain at least one number";
+  } else if (!/[a-z]/.test(values.newPassword)) {
+    errors.oldPassword = "Password should contain at least one lowercase";
+  } else if (!/[A-Z]/.test(values.newPassword)) {
+    errors.oldPassword = "Password should contain at least one uppercase";
+  } else if (!/\W/.test(values.newPassword)) {
+    errors.newPassword =
+      "Password should contain at least one special character";
+  }
+
+  // Validation for confirm password
+  if (!values.confirmPassword) {
+    //password - more than 8 characters
+    errors.confirmPassword = "Confirm Password is required";
+  } else if (values.newPassword.length < 8) {
+    errors.confirmPassword = "Password needs to be more than 8 characters";
+  } else if (!/\d/.test(values.confirmPassword)) {
+    errors.confirmPassword = "Password should contain at least one number";
+  } else if (!/[a-z]/.test(values.confirmPassword)) {
+    errors.confirmPassword = "Password should contain at least one lowercase";
+  } else if (!/[A-Z]/.test(values.confirmPassword)) {
+    errors.confirmPassword = "Password should contain at least one uppercase";
+  } else if (!/\W/.test(values.confirmPassword)) {
+    errors.confirmPassword =
+      "Password should contain at least one special character";
+  } else if (values.newPassword != values.confirmPassword) {
+    errors.confirmPassword =
+      "New Password and confirm password must be the same";
+  }
+
   return errors;
 }
