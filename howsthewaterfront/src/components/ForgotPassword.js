21f31ade416df3924c3c52e10c1b54072a810e8d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-08 14:47:17 -0500

    Forgot-password: Added form validation and footer

--- howsthewaterfront/src/components/ForgotPassword.js
+++ howsthewaterfront/src/components/ForgotPassword.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "./Header";
 import Search from "./Search";
+import Footer from "./Footer";
+import useForm from "../components/helper/useForm";
+import validate from "../components/helper/validateForgotPassword";
+import { withRouter } from "react-router-dom";
+import "../styles/forgot-password.css";
 
 function ForgotPassword(props) {
   return (
@@ -12,7 +17,18 @@ function ForgotPassword(props) {
       <div className="search-body">
         <Search />
       </div>
+      {/* FORGOT PASSWORD BODY */}
+      <div className="forgotPassword-body">
+        <div className="forgotPassword-content"></div>
+      </div>
+      {/* END OF FORGOT PASSWORD BODY */}
+
+      {/* FOOTER SECTION */}
+      <footer className="footer">
+        <Footer />
+      </footer>
+      {/* END OF FOOTER SECTION */}
     </div>
   );
 }
-export default ForgotPassword;
+export default withRouter(ForgotPassword);
