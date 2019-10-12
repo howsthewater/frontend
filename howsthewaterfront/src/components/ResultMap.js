3ecd856437b26527f48f8bafbf0fd14659740b40
jamesbieber <jamesbieber97@gmail.com>
2019-10-12 08:36:26 -0700

    ResultMap component completed

---
+++ howsthewaterfront/src/components/ResultMap.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDkCag5CYyBKqA99wjIvVdCUSxLR1IHRAY&callback=initMap",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: 37.263514, lng: -122.410365 }}
+  >
+    {props.isMarkerShown && (
+      <Marker position={{ lat: 37.263514, lng: -122.410365 }} />
+    )}
+  </GoogleMap>
+));
+
+class MapEx extends Component {
+  state = {
+    isMarkerShown: true
+  };
+
+  componentDidMount() {
+    this.delayedShowMarker();
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: false });
+    }, 3000);
+  };
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
+  };
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    );
+  }
+}
+
+export default MapEx;
