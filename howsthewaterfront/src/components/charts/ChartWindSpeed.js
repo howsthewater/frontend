284f858b50fee44d333652308ac80d9818ca3676
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-10-15 11:08:52 -0500

    Render-chart: Added dummy data for wind-speed for 10 days

--- howsthewaterfront/src/components/charts/ChartWindSpeed.js
+++ howsthewaterfront/src/components/charts/ChartWindSpeed.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-function ChartWindSpeed() {
+const ChartWindSpeed = () => {
+  // const windSpeedForecastQuery = (Enter the gql query here);
+
+  // TEMPORARY DATA FOR CHARTS
+  let windSpeedForecastLabel = [
+    "10/1",
+    "10/2",
+    "10/3",
+    "10/4",
+    "10/5",
+    "10/6",
+    "10/7",
+    "10/8",
+    "10/9",
+    "10/10"
+  ];
+  let windSpeedForecastData = [20, 30, 24, 12, 36, 10, 23, 19, 34, 10];
   return <div>Chart component</div>;
-}
+};
 
 export default ChartWindSpeed;
