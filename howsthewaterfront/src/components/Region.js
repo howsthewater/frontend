057521b17215f74d6d2d23ed7f6c65ff9c5155da
jeffyjkang <jeffyjkang@gmail.com>
2019-10-17 20:22:30 -0700

    bring in basic map component

--- howsthewaterfront/src/components/Region.js
+++ howsthewaterfront/src/components/Region.js
@@ -4,6 +4,42 @@ import Search from "./Search";
 import Footer from "./Footer";
 import { withRouter } from "react-router-dom";
 import "../styles/region.css";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+const MapComponent = compose(
+  withProps({
+    googleMapURL: process.env.REACT_APP_GOOGLE_API_KEY,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{
+      // hardcode for now
+      lat: 41.99,
+      lng: -124.21
+    }}
+  >
+    <Marker
+      position={{
+        // hardcode for now
+        lat: 41.99,
+        lng: -124.21
+      }}
+    />
+  </GoogleMap>
+));
 
 const Region = () => {
   return (
@@ -14,6 +50,12 @@ const Region = () => {
       </div>
       <div className="regionBody">
         {/* map component */}
+        <MapComponent
+          isMarkerShown="false"
+          onMarkerClick="false"
+          latitude="41.99"
+          longitude="-124.21"
+        />
         <footer className="footer">
           <Footer />
         </footer>
