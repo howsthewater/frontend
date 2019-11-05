b6b27fa3bbab9fccb30d58af5251505b4da864c5
stackblock <lambdatl.anthony@gmail.com>
2019-11-04 19:49:50 -0500

    sw5

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Amplify from "aws-amplify";
 import { BrowserRouter as Router } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import * as serviceWorker from './serviceWorker';
 require("dotenv").config();
 
 const client = new ApolloClient({
